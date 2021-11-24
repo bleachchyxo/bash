538978abf668956d25d42ccbb463adeae07930f7
bleachchyxo <85224206+bleachchyxo@users.noreply.github.com>
2021-11-24 10:19:02 -0600

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+clear
+echo
+echo
+echo "		                 #####   ####"
+echo "		                ####### ######"
+echo "		                ####### ######"
+echo "		         #####   ###### #####    #####"
+echo "		        ########  ##### ####  #########"
+echo "		        ###########  ## ###  ##########"
+echo "		           ########## # # ##########"
+echo
+echo "		       ########   ##########   ########"
+echo "		      ###    ###      ##      ###    ###"
+echo "		      ###             ##      ###"
+echo "		      ### ######      ##      ###"
+echo "		      ###    ###      ##      ###    ###"
+echo "		       ########   ##########   ########"
+echo
+echo "                     Graphic Industry Corporation (C) 2021"
+echo
+echo
+echo "                          Press any key to continue"
+echo
+echo
+read -n 1 -s -r -p ""
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo "                               Developed by chuxo"
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+read -n 1 -s -r -p ""
+clear
+echo "hola"
